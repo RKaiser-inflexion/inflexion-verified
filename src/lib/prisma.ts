39d1956028c391b9615d3baee6bb1b01e7fc6692
RKaiser-inflexion <richard@inflexion.cz>
2026-07-10 14:38:52 +0200

    Automatically enforce connection pooling to speed up Prisma on Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,16 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const url = process.env.DATABASE_URL;
+const pooledUrl = url && !url.includes('pgbouncer=true') ? `${url}&pgbouncer=true` : url;
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: pooledUrl,
+    },
+  },
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
